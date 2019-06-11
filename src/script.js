12973682a3670b74db0953f42e4ac841aab00e2e
Andres Delgado <adelgado0723@gmail.com>
2019-06-11 17:05:43 -0400

    Made Several Changes
    - Fixed script.js to not depend on JQuery
    - Changed variables to block-level variables
    - Changed HTML to not use a table for laying out the grid

--- src/script.js
+++ src/script.js
@@ -1,68 +1,84 @@
-var $body = $('body');
-var $this = $(this);
-var $galleryModal = $('.gallery-modal');
-var $galleryModalItem = $('.gallery-item');
+const modalClose = document.querySelector('.modal-close');
+const galleryItem = document.querySelectorAll('.gallery-item');
+const galleryModal = document.querySelector('.gallery-modal');
 
-projObj = [
+const projectName = document.querySelector('.project-name');
+const projectDesc = document.querySelector('.project-desc');
+const projectContent = document.querySelector('.project-content');
+
+const projObj = [
   {
     name: 'Project 1',
     label: 'Enterprise pencil solution',
     info: 'Project 1 Description',
+    images: [
+      'http://via.placeholder.com/250x250',
+      'http://via.placeholder.com/250x250',
+    ],
   },
   {
     name: 'Project 2',
     label: 'A mobile adventure',
     info: 'Project 2 Description',
+    images: [
+      'http://via.placeholder.com/250x250',
+      'http://via.placeholder.com/250x250',
+    ],
   },
   {
     name: 'Project 3',
     label: 'Hacking',
     info: 'Project 3 Description',
+    images: [
+      'http://via.placeholder.com/250x250',
+      'http://via.placeholder.com/250x250',
+      'http://via.placeholder.com/250x250',
+      'http://via.placeholder.com/250x250',
+      'http://via.placeholder.com/250x250',
+      'http://via.placeholder.com/250x250',
+    ],
   },
   {
     name: 'Project 4',
     label: 'Read more about project 4',
     info: 'Project 4 Description',
+    images: [
+      'http://via.placeholder.com/250x250',
+      'http://via.placeholder.com/250x250',
+    ],
   },
   {
     name: 'Project 5',
     info: 'Project 5 Description',
+    images: [
+      'http://via.placeholder.com/250x250',
+      'http://via.placeholder.com/250x250',
+    ],
   },
 ];
 
-/*
- * Loop through asset folders with matching names. Commented out to show with placeholder images.
- */
-var i;
-var projectImg;
-var projectLabel;
-// Commented out for demo purposes
-// for (i = 1; i < 5; i++) {
-//     projectImg += "<td><img src='img/project-" + i + "/img" + i + ".png'></td>";
-// }
-for (i = 1; i < 5; i++) {
-  projectImg += "<td><img src='http://via.placeholder.com/250x250'></td>";
-}
-$galleryModalItem.each(function(index) {
-  var dataProj = $(this).data('project');
-  $(this)
-    .find('.gallery-label > h2')
-    .text(projObj[index].name);
+galleryItem.forEach((item, index) => {
+  item.querySelector('.gallery-label > h2').textContent = projObj[index].name;
 
-  $(this)
-    .find('.gallery-sublabel')
-    .text(projObj[index].label);
+  item.querySelector('.gallery-sublabel').textContent = projObj[index].label;
 
-  $(this).on('click', function() {
-    $('.project-name').html(projObj[index].name);
-    $('.project-desc').html(projObj[index].info);
-    $('.project-row').html(projectImg);
-    $galleryModal.show();
-    $body.css('overflow-y', 'hidden');
+  item.addEventListener('click', () => {
+    projectName.textContent = projObj[index].name;
+    projectDesc.textContent = projObj[index].info;
+
+    let projectImages = '';
+    projObj[index].images.forEach((imgURL) => {
+      projectImages += `<img src=${imgURL}>`;
+    });
+    projectContent.innerHTML = projectImages;
+
+    galleryModal.classList.add('is-visible');
   });
+
+  // $(document.querySelector('body')).css('overflow-y', 'hidden');
 });
 
-$('.modal-close').on('click', function() {
-  $galleryModal.fadeOut();
-  $body.css('overflow-y', 'auto');
+modalClose.addEventListener('click', () => {
+  galleryModal.classList.remove('is-visible');
+  // body.setAttribute('overflow-y', 'auto');
 });
