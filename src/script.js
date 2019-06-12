63bf078065a7fcf157466446938902061ee7592d
Andres Delgado <adelgado0723@gmail.com>
2019-06-12 17:16:45 -0400

    Changed how projects get populated into HTML
    - Changed gallery item background image to be part of the projects
    object
    - placed gallery items in CSS Grid
    - Added box-shadow styling to gallery items

--- src/script.js
+++ src/script.js
@@ -1,12 +1,12 @@
 const modalClose = document.querySelector('.modal-close');
 const galleryItem = document.querySelectorAll('.gallery-item');
+const galleryContainer = document.querySelector('.gallery-container');
 const galleryModal = document.querySelector('.gallery-modal');
-
 const projectName = document.querySelector('.project-name');
 const projectDesc = document.querySelector('.project-desc');
 const projectContent = document.querySelector('.project-content');
 
-const projObj = [
+const projects = [
   {
     name: 'Project 1',
     label: 'Enterprise pencil solution',
@@ -15,6 +15,7 @@ const projObj = [
       'http://via.placeholder.com/250x250',
       'http://via.placeholder.com/250x250',
     ],
+    background: 'https://picsum.photos/id/555/800/800',
   },
   {
     name: 'Project 2',
@@ -24,6 +25,7 @@ const projObj = [
       'http://via.placeholder.com/250x250',
       'http://via.placeholder.com/250x250',
     ],
+    background: 'https://picsum.photos/id/528/800/800',
   },
   {
     name: 'Project 3',
@@ -37,6 +39,7 @@ const projObj = [
       'http://via.placeholder.com/250x250',
       'http://via.placeholder.com/250x250',
     ],
+    background: 'https://picsum.photos/id/657/800/800',
   },
   {
     name: 'Project 4',
@@ -46,35 +49,101 @@ const projObj = [
       'http://via.placeholder.com/250x250',
       'http://via.placeholder.com/250x250',
     ],
+    background: 'https://picsum.photos/id/487/800/800',
   },
   {
     name: 'Project 5',
+    label: 'Read more about project 5',
     info: 'Project 5 Description',
     images: [
       'http://via.placeholder.com/250x250',
       'http://via.placeholder.com/250x250',
     ],
+    background: 'https://picsum.photos/id/481/800/800',
+  },
+  {
+    name: 'Project 6',
+    info: 'Project 6 Description',
+    label: 'Read more about project 6',
+    images: [
+      'http://via.placeholder.com/250x250',
+      'http://via.placeholder.com/250x250',
+    ],
+    background: 'https://picsum.photos/id/482/800/800',
+  },
+  {
+    name: 'Project 7',
+    info: 'Project 7 Description',
+    label: 'Read more about project 7',
+    images: [
+      'http://via.placeholder.com/250x250',
+      'http://via.placeholder.com/250x250',
+    ],
+    background: 'https://picsum.photos/id/483/800/800',
+  },
+  {
+    name: 'Project 8',
+    info: 'Project 8 Description',
+    label: 'Read more about project 8',
+    images: [
+      'http://via.placeholder.com/250x250',
+      'http://via.placeholder.com/250x250',
+    ],
+    background: 'https://picsum.photos/id/455/800/800',
   },
 ];
 
-galleryItem.forEach((item, index) => {
-  item.querySelector('.gallery-label > h2').textContent = projObj[index].name;
+// galleryItem.forEach((item, index) => {
+//   item.querySelector('.gallery-label > h2').textContent = projects[index].name;
+//   item.querySelector(
+//     '.img'
+//   ).style.backgroundImage = `url(${projects[index].background})`;
+//   item.querySelector('.gallery-sublabel').textContent = projects[index].label;
 
-  item.querySelector('.gallery-sublabel').textContent = projObj[index].label;
+//   item.addEventListener('click', () => {
+//     projectName.textContent = projects[index].name;
+//     projectDesc.textContent = projects[index].info;
 
-  item.addEventListener('click', () => {
-    projectName.textContent = projObj[index].name;
-    projectDesc.textContent = projObj[index].info;
+//     let projectImages = '';
+//     projects[index].images.forEach((imgURL) => {
+//       projectImages += `<img src=${imgURL}>`;
+//     });
+//     projectContent.innerHTML = projectImages;
 
+//     galleryModal.classList.add('is-visible');
+//   });
+
+projects.forEach((project) => {
+  const projectEntry = document.createElement('div');
+  projectEntry.classList.add('gallery-item');
+
+  projectEntry.innerHTML = `     
+            <div class="img-container">
+              <div class="img">
+                <div class="img-overlay"></div>
+                <div class="gallery-label">
+                  <h2>${project.name}</h2>
+                  <span class="gallery-sublabel">${project.label}</span>
+                </div>
+              </div>
+            </div>
+    `;
+
+  const background = projectEntry.querySelector('.img');
+  background.style.backgroundImage = `url(${project.background})`;
+
+  projectEntry.addEventListener('click', () => {
+    projectName.textContent = project.name;
+    projectDesc.textContent = project.info;
     let projectImages = '';
-    projObj[index].images.forEach((imgURL) => {
+    project.images.forEach((imgURL) => {
       projectImages += `<img src=${imgURL}>`;
     });
     projectContent.innerHTML = projectImages;
-
     galleryModal.classList.add('is-visible');
   });
 
+  galleryContainer.appendChild(projectEntry);
   // $(document.querySelector('body')).css('overflow-y', 'hidden');
 });
 
