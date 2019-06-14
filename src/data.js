182a8aaf5a9d192af8eb5e54b006827fe6de33a4
Andres Delgado <adelgado0723@gmail.com>
2019-06-14 17:25:08 -0400

    Started working on carousel in modal overlay

---
+++ src/data.js
@@ -0,0 +1,90 @@
+const Data = {
+  projects: [
+    {
+      name: 'Project 1',
+      label: 'Enterprise pencil solution',
+      info: 'Project 1 Description',
+      images: [
+        'http://via.placeholder.com/250x250',
+        'http://via.placeholder.com/250x250',
+      ],
+      background: 'https://picsum.photos/id/555/800/800',
+    },
+    {
+      name: 'Project 2',
+      label: 'A mobile adventure',
+      info: 'Project 2 Description',
+      images: [
+        'http://via.placeholder.com/250x250',
+        'http://via.placeholder.com/250x250',
+      ],
+      background: 'https://picsum.photos/id/528/800/800',
+    },
+    {
+      name: 'Project 3',
+      label: 'Hacking',
+      info: 'Project 3 Description',
+      images: [
+        'http://via.placeholder.com/250x250',
+        'http://via.placeholder.com/250x250',
+        'http://via.placeholder.com/250x250',
+        'http://via.placeholder.com/250x250',
+        'http://via.placeholder.com/250x250',
+        'http://via.placeholder.com/250x250',
+      ],
+      background: 'https://picsum.photos/id/657/800/800',
+    },
+    {
+      name: 'Project 4',
+      label: 'Read more about project 4',
+      info: 'Project 4 Description',
+      images: [
+        'http://via.placeholder.com/250x250',
+        'http://via.placeholder.com/250x250',
+      ],
+      background: 'https://picsum.photos/id/487/800/800',
+    },
+    {
+      name: 'Project 5',
+      label: 'Read more about project 5',
+      info: 'Project 5 Description',
+      images: [
+        'http://via.placeholder.com/250x250',
+        'http://via.placeholder.com/250x250',
+      ],
+      background: 'https://picsum.photos/id/481/800/800',
+    },
+    {
+      name: 'Project 6',
+      info: 'Project 6 Description',
+      label: 'Read more about project 6',
+      images: [
+        'http://via.placeholder.com/250x250',
+        'http://via.placeholder.com/250x250',
+      ],
+      background: 'https://picsum.photos/id/482/800/800',
+    },
+    {
+      name: 'Project 7',
+      info: 'Project 7 Description',
+      label: 'Read more about project 7',
+      images: [
+        'http://via.placeholder.com/250x250',
+        'http://via.placeholder.com/250x250',
+      ],
+      background: 'https://picsum.photos/id/483/800/800',
+    },
+    {
+      name: 'Project 8',
+      info: 'Project 8 Description',
+      label: 'Read more about project 8',
+      images: [
+        'http://via.placeholder.com/250x250',
+        'http://via.placeholder.com/250x250',
+      ],
+      background: 'https://picsum.photos/id/455/800/800',
+    },
+  ],
+};
+
+export default Data;
--- src/script.js
+++ src/script.js
@@ -1,103 +1,38 @@
+import Data from './data.js';
 const modalClose = document.querySelector('.modal-close');
 const galleryContainer = document.querySelector('.gallery-container');
 const galleryModal = document.querySelector('.gallery-modal');
-
 const galleryModalBody = document.querySelector('.gallery-modal-body');
 const projectName = document.querySelector('.project-name');
 const projectDesc = document.querySelector('.project-desc');
 const projectContent = document.querySelector('.project-content');
 
-const projects = [
-  {
-    name: 'Project 1',
-    label: 'Enterprise pencil solution',
-    info: 'Project 1 Description',
-    images: [
-      'http://via.placeholder.com/250x250',
-      'http://via.placeholder.com/250x250',
-    ],
-    background: 'https://picsum.photos/id/555/800/800',
-  },
-  {
-    name: 'Project 2',
-    label: 'A mobile adventure',
-    info: 'Project 2 Description',
-    images: [
-      'http://via.placeholder.com/250x250',
-      'http://via.placeholder.com/250x250',
-    ],
-    background: 'https://picsum.photos/id/528/800/800',
-  },
-  {
-    name: 'Project 3',
-    label: 'Hacking',
-    info: 'Project 3 Description',
-    images: [
-      'http://via.placeholder.com/250x250',
-      'http://via.placeholder.com/250x250',
-      'http://via.placeholder.com/250x250',
-      'http://via.placeholder.com/250x250',
-      'http://via.placeholder.com/250x250',
-      'http://via.placeholder.com/250x250',
-    ],
-    background: 'https://picsum.photos/id/657/800/800',
-  },
-  {
-    name: 'Project 4',
-    label: 'Read more about project 4',
-    info: 'Project 4 Description',
-    images: [
-      'http://via.placeholder.com/250x250',
-      'http://via.placeholder.com/250x250',
-    ],
-    background: 'https://picsum.photos/id/487/800/800',
-  },
-  {
-    name: 'Project 5',
-    label: 'Read more about project 5',
-    info: 'Project 5 Description',
-    images: [
-      'http://via.placeholder.com/250x250',
-      'http://via.placeholder.com/250x250',
-    ],
-    background: 'https://picsum.photos/id/481/800/800',
-  },
-  {
-    name: 'Project 6',
-    info: 'Project 6 Description',
-    label: 'Read more about project 6',
-    images: [
-      'http://via.placeholder.com/250x250',
-      'http://via.placeholder.com/250x250',
-    ],
-    background: 'https://picsum.photos/id/482/800/800',
-  },
-  {
-    name: 'Project 7',
-    info: 'Project 7 Description',
-    label: 'Read more about project 7',
-    images: [
-      'http://via.placeholder.com/250x250',
-      'http://via.placeholder.com/250x250',
-    ],
-    background: 'https://picsum.photos/id/483/800/800',
-  },
-  {
-    name: 'Project 8',
-    info: 'Project 8 Description',
-    label: 'Read more about project 8',
-    images: [
-      'http://via.placeholder.com/250x250',
-      'http://via.placeholder.com/250x250',
-    ],
-    background: 'https://picsum.photos/id/455/800/800',
-  },
-];
+function handleGalleryClick(event) {
+  //refresh carousel
+  console.log('Click event hooked up', event);
+}
 
-projects.forEach((project) => {
+// Creating and storing an img el element for
+// each image attached to each project
+Data.projects.forEach(function createImgElements(project) {
+  project.imageElements = project.images.map(function makeCarouselItem(
+    url,
+    index
+  ) {
+    const carouselImage = document.createElement('img');
+    carouselImage.src = url;
+    carouselImage.alt = project.name + ' Image';
+    carouselImage.setAttribute('data-index', index);
+    carouselImage.addEventListener('click', handleGalleryClick);
+    return carouselImage;
+  });
+});
+
+Data.projects.forEach((project) => {
   const projectEntry = document.createElement('div');
   projectEntry.classList.add('gallery-item');
 
+  // TODO : create this array and store it on loading
   projectEntry.innerHTML = `     
             <div class="img-container">
               <div class="img">
@@ -113,14 +48,22 @@ projects.forEach((project) => {
   const background = projectEntry.querySelector('.img');
   background.style.backgroundImage = `url(${project.background})`;
 
-  projectEntry.addEventListener('click', () => {
+  projectEntry.addEventListener('click', function addProjectClickListener() {
     projectName.textContent = project.name;
     projectDesc.textContent = project.info;
-    let projectImages = '';
-    project.images.forEach((imgURL) => {
-      projectImages += `<img src=${imgURL}>`;
+    projectContent.innerHTML = '';
+    // Set the active image
+
+    // Setting up the carousel images
+    project.imageElements.forEach(function attachCarouselImages(image) {
+      projectContent.appendChild(image);
     });
-    projectContent.innerHTML = projectImages;
+    // const carousel = `
+    //   <div class="carousel">
+    //   </div>
+    // `;
+
+    // TODO : remove one of these
     galleryModal.classList.add('is-visible');
     galleryModalBody.classList.add('is-visible');
   });
@@ -129,9 +72,9 @@ projects.forEach((project) => {
   // document.querySelector('body').setAttribute('overflow-y', 'hidden');
 });
 
-modalClose.addEventListener('click', () => {
+modalClose.addEventListener('click', function addCloseButtonListener() {
+  // TODO : remove one of these
   galleryModal.classList.remove('is-visible');
-
   galleryModalBody.classList.remove('is-visible');
   // document.querySelector('body').setAttribute('overflow-y', 'auto');
 });
